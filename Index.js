bd81c34736d90fe8169ddb713d926b70dec2c6be
Viskto <webdev.vitor@gmail.com>
2022-09-19 16:55:23 -0300

    Add ability to write the player symbol to the square

--- Index.js
+++ Index.js
@@ -4,14 +4,13 @@ const nameInput = document.querySelector("input");
 
 const game = {
   i: 0,
-  currentPlayer: game.board.players[i],
   board: {
     squares: [],
     players: [],
   },
 
   playerFactory: (name) => {
-    if (game.players[0]) {
+    if (game.board.players[0]) {
       /* Because if we click the button to create new player,
        * The new player object will always be immediately pushed
        * into the gameBoard.players array. */
@@ -29,7 +28,7 @@ const game = {
 };
 
 newPlayerButton.addEventListener("click", () => {
-  if (game.board.players.length < 3) {
+  if (game.board.players.length < 2) {
     game.board.players.push(game.playerFactory(nameInput.value));
     nameInput.value = "";
   } else alert("Only two players allowed for now");
@@ -39,8 +38,8 @@ boardSquares.forEach((square) => {
   game.board.squares.push(square);
   square.addEventListener("click", function () {
     if (!this.symbol) {
-      this.symbol = currentPlayer.symbol;
-      this.textContent = currentPlayer.symbol;
+      this.symbol = game.board.players[game.i].symbol;
+      this.textContent = game.board.players[game.i].symbol;
       if (game.i === 0) {
         game.i = 1;
       } else game.i = 0;
