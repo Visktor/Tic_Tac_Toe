26ea69b82afe6fb6614f5ecaaff1392ef007596d
Viskto <webdev.vitor@gmail.com>
2022-09-19 14:46:17 -0300

    Add a div to keep score

--- Index.js
+++ Index.js
@@ -1,3 +1,20 @@
 const boardSquares = document.querySelectorAll(".inner");
 
+const gameBoard = {
+  squares: [],
+  players: [],
+};
 
+const playerFactory = (name) => {
+  if (gameBoard.players.length > 3) {
+    if (gameBoard.players[0]) {
+      /* Because if we click the button to create new player,
+       * The new player object will always be immediately pushed
+       * into the gameBoard.players object. */
+      symbol = "O";
+    } else {
+      symbol = "X";
+    }
+  }
+  return { name, symbol };
+};
