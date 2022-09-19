d1e7eb7840a5735ce33c7e8990da31df7daf5eb1
Viskto <webdev.vitor@gmail.com>
2022-09-19 15:27:36 -0300

    Add player creator event listener

--- Index.js
+++ Index.js
@@ -1,4 +1,6 @@
 const boardSquares = document.querySelectorAll(".inner");
+const newPlayerButton = document.querySelector(".addPlayer");
+const nameInput = document.querySelector("input");
 
 const gameBoard = {
   squares: [],
@@ -6,15 +8,21 @@ const gameBoard = {
 };
 
 const playerFactory = (name) => {
-  if (gameBoard.players.length > 3) {
-    if (gameBoard.players[0]) {
-      /* Because if we click the button to create new player,
-       * The new player object will always be immediately pushed
-       * into the gameBoard.players object. */
-      symbol = "O";
-    } else {
-      symbol = "X";
-    }
+  if (gameBoard.players[0]) {
+    /* Because if we click the button to create new player,
+     * The new player object will always be immediately pushed
+     * into the gameBoard.players array. */
+    symbol = "O";
+  } else {
+    symbol = "X";
   }
+
   return { name, symbol };
 };
+
+newPlayerButton.addEventListener("click", () => {
+  if (gameBoard.players.length < 3) {
+    gameBoard.players.push(playerFactory(nameInput.value));
+    nameInput.value = "";
+  }
+});
