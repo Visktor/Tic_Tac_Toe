3db4f854958886d3bfbf2ebe4403475d37da45d9
Viskto <webdev.vitor@gmail.com>
2022-09-19 16:39:55 -0300

    Add board squares event listener

--- Index.js
+++ Index.js
@@ -2,27 +2,48 @@ const boardSquares = document.querySelectorAll(".inner");
 const newPlayerButton = document.querySelector(".addPlayer");
 const nameInput = document.querySelector("input");
 
-const gameBoard = {
-  squares: [],
-  players: [],
-};
+const game = {
+  i: 0,
+  currentPlayer: game.board.players[i],
+  board: {
+    squares: [],
+    players: [],
+  },
 
-const playerFactory = (name) => {
-  if (gameBoard.players[0]) {
-    /* Because if we click the button to create new player,
-     * The new player object will always be immediately pushed
-     * into the gameBoard.players array. */
-    symbol = "O";
-  } else {
-    symbol = "X";
-  }
+  playerFactory: (name) => {
+    if (game.players[0]) {
+      /* Because if we click the button to create new player,
+       * The new player object will always be immediately pushed
+       * into the gameBoard.players array. */
+      symbol = "O";
+    } else {
+      symbol = "X";
+    }
 
-  return { name, symbol };
+    return { name, symbol };
+  },
+
+  // checkForMatch: () =>{
+  //   if (game.board.squares)
+  // }
 };
 
 newPlayerButton.addEventListener("click", () => {
-  if (gameBoard.players.length < 3) {
-    gameBoard.players.push(playerFactory(nameInput.value));
+  if (game.board.players.length < 3) {
+    game.board.players.push(game.playerFactory(nameInput.value));
     nameInput.value = "";
-  }
+  } else alert("Only two players allowed for now");
+});
+
+boardSquares.forEach((square) => {
+  game.board.squares.push(square);
+  square.addEventListener("click", function () {
+    if (!this.symbol) {
+      this.symbol = currentPlayer.symbol;
+      this.textContent = currentPlayer.symbol;
+      if (game.i === 0) {
+        game.i = 1;
+      } else game.i = 0;
+    }
+  });
 });
