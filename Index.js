317c70ccc4f3b37064d1d39a07cdb07933702b9e
Viskto <webdev.vitor@gmail.com>
2022-09-20 20:42:13 -0300

    Add checkForMatch function

--- Index.js
+++ Index.js
@@ -16,16 +16,50 @@ const game = {
        * into the gameBoard.players array. */
       symbol = "O";
     } else {
-      symbol = "X";
+      symbol = "O";
     }
 
     return { name, symbol };
   },
-
-  // checkForMatch: () =>{
-  //   if (game.board.squares)
-  // }
 };
+
+function checkForMatch() {
+  let Xarray = [];
+  let Oarray = [];
+
+  game.board.squares.forEach((square) => {
+    if (square.symbol) {
+      if (square.symbol === "X") {
+        Xarray.push(game.board.squares.indexOf(square));
+      } else if (square.symbol === "O") {
+        Oarray.push(game.board.squares.indexOf(square));
+      }
+    }
+  });
+  (() => {
+    if (
+      (Xarray.includes(0) && Xarray.includes(1) && Xarray.includes(2)) ||
+      (Xarray.includes(0) && Xarray.includes(3) && Xarray.includes(6)) ||
+      (Xarray.includes(0) && Xarray.includes(4) && Xarray.includes(8)) ||
+      (Xarray.includes(1) && Xarray.includes(4) && Xarray.includes(7)) ||
+      (Xarray.includes(2) && Xarray.includes(5) && Xarray.includes(8)) ||
+      (Xarray.includes(3) && Xarray.includes(4) && Xarray.includes(5)) ||
+      (Xarray.includes(6) && Xarray.includes(7) && Xarray.includes(8))
+    ) {
+      alert("player 1 wins");
+    } else if (
+      (Oarray.includes(0) && Oarray.includes(1) && Oarray.includes(2)) ||
+      (Oarray.includes(0) && Oarray.includes(3) && Oarray.includes(6)) ||
+      (Oarray.includes(0) && Oarray.includes(4) && Oarray.includes(8)) ||
+      (Oarray.includes(1) && Oarray.includes(4) && Oarray.includes(7)) ||
+      (Oarray.includes(2) && Oarray.includes(5) && Oarray.includes(8)) ||
+      (Oarray.includes(3) && Oarray.includes(4) && Oarray.includes(5)) ||
+      (Oarray.includes(6) && Oarray.includes(7) && Oarray.includes(8))
+    ) {
+      alert("player 2 wins");
+    }
+  })();
+}
 
 newPlayerButton.addEventListener("click", () => {
   if (game.board.players.length < 2) {
@@ -41,6 +75,7 @@ boardSquares.forEach((square) => {
       this.symbol = game.board.players[game.i].symbol;
       this.textContent = game.board.players[game.i].symbol;
     }
+    checkForMatch();
     if (game.i === 0) {
       game.i = 1;
     } else game.i = 0;
